d9ac70e59f5dcaa1d2b46ea530a84d163a299fe3
Olivia Perry <153868993+oliviaperry1997@users.noreply.github.com>
2025-10-31 18:11:20 +0000

    Remove astrojs/node adapter since output is static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,8 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   integrations: [react(), tailwind()],
-
-  adapter: node({
-    mode: 'standalone'
-  })
 });
